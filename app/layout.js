5025c2b983ecfb8d66660ff2b9db6acdfc160189
munashex <munashemugondaa@gmail.com>
2024-08-08 13:52:14 +0200

    adding web analytics

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer'
-
+import { Analytics } from '@vercel/analytics/react';
 
 
 
@@ -37,6 +37,7 @@ export default function RootLayout({ children }) {
         <div className="w-fit overflow-x-hidden">
         <Navbar/>
         {children}
+        <Analytics/>
         <Footer/>
         </div>
         </body>
